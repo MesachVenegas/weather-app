3bf58fa9c74b5cf49a7ce81fcfc93998619c723e
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-01-03 00:52:59 -0800

    addÑ loader

--- src/Modules/Card/Card.jsx
+++ src/Modules/Card/Card.jsx
@@ -9,50 +9,58 @@ const Card = () => {
     const [data, setData] = useState("");
     const [backgroundImage, setBackgroundImage] = useState(backgrounds.default)
     const [units, setUnits] = useState(true);
+    const [current, setCurrent] = useState('')
 
     // Obtengo la ubicación del usuario.
     const success = async (pos) => {
         const crd = await pos.coords
-        await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${crd.latitude}&lon=${crd.longitude}&appid=${key}&units=${units ? 'metric' : 'imperial'}&lang=es`)
+        await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${crd.latitude}&lon=${crd.longitude}&appid=${key}&lang=es`)
             .then(res => setData(res.data))
             .catch(error => console.log(error));
     }
 
+    // console.log(backgroundImage);
     useEffect( () =>{
         navigator.geolocation.getCurrentPosition(success, error, options);
     },[])
+
 
     const convert = () => {
         setUnits(!units)
         navigator.geolocation.getCurrentPosition(success, error, options);
     };
 
-    console.log(data);
-
     const loadPanel = () => {
         if (!data){
             return (
-                <h1>Loading ...</h1>
-            )
+                <>
+                    <div className="loader__container">
+                        <span class="loader"></span>
+                    </div>
+                </>
+                )
         }else{
             return(
                 <>
                     <div className="weather__img">
                         <img src={`http://openweathermap.org/img/wn/${data.weather?.[0].icon}@2x.png`} alt="" />
                         <h2 className='city__status'>{data.weather?.[0].description}</h2>
+                        <p className="weather__current">
+                            {data.main?.temp}
+                        </p>
                     </div>
                     <div className="weather__head">
                         <h1 className='city'>{data.name}, {data.sys?.country}</h1>
                     </div>
-                    <div className='weather__data'>
-                        <p><i className="fa-solid fa-temperature-three-quarters"></i> {data.main?.temp} {units ? '°C' : '°F'}</p>
-                        <p>min {data.main?.temp_min} {units ? '°C' : '°F'}</p>
-                        <p>max {data.main?.temp_max} {units ? '°C' : '°F'}</p>
-                        <p>Sensacion: {data.main?.feels_like} {units ? '°C' : '°F'}</p>
-                        <p><i className="fa-solid fa-droplet"></i> {data.main?.humidity}%</p>
+                    <div className="data__temp">
+                        <button onClick={convert}><i class="fa-solid fa-rotate"></i></button>
+                        <p>Min/Max: {data.main?.temp_min} / {data.main?.temp_max}</p>
+                        <p>Sensacion: {data.main?.feels_like}</p>
+                    </div>
+                    <div className="temp__atmosphere">
+                        <p>Humedad: {data.main?.humidity}%</p>
                         <p>Presion: {data.main?.pressure}hPa</p>
                     </div>
-                    <button onClick={convert}>Change</button>
                 </>
             )
         }
@@ -61,8 +69,6 @@ const Card = () => {
     document.body.style.backgroundImage = `url(${backgroundImage})`;
     document.body.style.backgroundSize = "cover";
 
-    // console.log(data);
-    // console.log(data.name);
     return (
         <div className='card'>
             {loadPanel()}
--- src/js/functions.mjs
+++ src/js/functions.mjs
@@ -11,8 +11,8 @@ export function error(err) {
 
 export const backgrounds = {
     default: 'https://images.theconversation.com/files/442675/original/file-20220126-17-1i0g402.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=675.0&fit=crop',
-    clouds: 'https://images.unsplash.com/photo-1599209248411-5124adbb1da2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-    rain: 'https://www.freevector.com/rain-background#',
-    wind: 'https://www.schaeffler.mx/remotemedien/media/_shared_media_rwd/04_sectors_1/industry_1/windpower_1/47120_header-schaeffler-industry-solutions-wind-wind.jpg',
-    snow: 'https://cdn.britannica.com/62/187062-138-9D11F50A/snowflakes.jpg?w=800&h=450&c=crop'
+    Clouds: 'https://images.unsplash.com/photo-1599209248411-5124adbb1da2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+    Rain: 'https://www.freevector.com/rain-background#',
+    Wind: 'https://www.schaeffler.mx/remotemedien/media/_shared_media_rwd/04_sectors_1/industry_1/windpower_1/47120_header-schaeffler-industry-solutions-wind-wind.jpg',
+    Snow: 'https://cdn.britannica.com/62/187062-138-9D11F50A/snowflakes.jpg?w=800&h=450&c=crop'
 }
