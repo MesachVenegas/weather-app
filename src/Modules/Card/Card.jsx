1283c242555061374e4a0e27576f6c62ea3f240d
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-01-02 23:16:42 -0800

    add: grid areas

--- src/Modules/Card/Card.jsx
+++ src/Modules/Card/Card.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import axios from 'axios';
-import { options, error } from '../../js/functions.mjs';
+import { options, error, backgrounds } from '../../js/functions.mjs';
 import { useState, useEffect } from 'react';
 import './Card.css';
 
 const Card = () => {
     const key = "f5c51686bf9b67fbaa05482bffad8ac4"
     const [data, setData] = useState("");
+    const [backgroundImage, setBackgroundImage] = useState(backgrounds.default)
     const [units, setUnits] = useState(true);
 
     // Obtengo la ubicación del usuario.
@@ -26,6 +27,8 @@ const Card = () => {
         navigator.geolocation.getCurrentPosition(success, error, options);
     };
 
+    console.log(data);
+
     const loadPanel = () => {
         if (!data){
             return (
@@ -34,26 +37,28 @@ const Card = () => {
         }else{
             return(
                 <>
-                    <div className="weather__head">
-                        <h1 className='city'>{data.name}, {data.sys?.country}</h1>
+                    <div className="weather__img">
+                        <img src={`http://openweathermap.org/img/wn/${data.weather?.[0].icon}@2x.png`} alt="" />
                         <h2 className='city__status'>{data.weather?.[0].description}</h2>
                     </div>
-                    <ul className='weather__data'>
-                        <li><i className="fa-solid fa-temperature-three-quarters"></i> {data.main?.temp} {units ? '°C' : '°F'}</li>
-                        <li>min {data.main?.temp_min} {units ? '°C' : '°F'}</li>
-                        <li>max {data.main?.temp_max} {units ? '°C' : '°F'}</li>
-                        <li>Sensacion: {data.main?.feels_like} {units ? '°C' : '°F'}</li>
-                        <li><i className="fa-solid fa-water"></i> {data.main?.sea_level}</li>
-                        <li><i className="fa-solid fa-droplet"></i> {data.main?.humidity}%</li>
-                        <li>Presion: {data.main?.pressure}hPa</li>
-                    </ul>
+                    <div className="weather__head">
+                        <h1 className='city'>{data.name}, {data.sys?.country}</h1>
+                    </div>
+                    <div className='weather__data'>
+                        <p><i className="fa-solid fa-temperature-three-quarters"></i> {data.main?.temp} {units ? '°C' : '°F'}</p>
+                        <p>min {data.main?.temp_min} {units ? '°C' : '°F'}</p>
+                        <p>max {data.main?.temp_max} {units ? '°C' : '°F'}</p>
+                        <p>Sensacion: {data.main?.feels_like} {units ? '°C' : '°F'}</p>
+                        <p><i className="fa-solid fa-droplet"></i> {data.main?.humidity}%</p>
+                        <p>Presion: {data.main?.pressure}hPa</p>
+                    </div>
                     <button onClick={convert}>Change</button>
                 </>
             )
         }
     }
 
-    document.body.style.backgroundImage = 'url(https://images.unsplash.com/photo-1599209248411-5124adbb1da2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80)';
+    document.body.style.backgroundImage = `url(${backgroundImage})`;
     document.body.style.backgroundSize = "cover";
 
     // console.log(data);
--- src/js/functions.mjs
+++ src/js/functions.mjs
@@ -7,3 +7,12 @@ export const options = {
 export function error(err) {
     console.warn(`ERROR(${err.code}): ${err.message}`);
 }
+//https://www.freevector.com/uploads/vector/preview/7040/FreeVector-Rain-Background.jpg
+
+export const backgrounds = {
+    default: 'https://images.theconversation.com/files/442675/original/file-20220126-17-1i0g402.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=675.0&fit=crop',
+    clouds: 'https://images.unsplash.com/photo-1599209248411-5124adbb1da2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+    rain: 'https://www.freevector.com/rain-background#',
+    wind: 'https://www.schaeffler.mx/remotemedien/media/_shared_media_rwd/04_sectors_1/industry_1/windpower_1/47120_header-schaeffler-industry-solutions-wind-wind.jpg',
+    snow: 'https://cdn.britannica.com/62/187062-138-9D11F50A/snowflakes.jpg?w=800&h=450&c=crop'
+}
